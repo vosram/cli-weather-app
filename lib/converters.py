e78fee81b0b9b95cd8cf441fc8734fbca30873d5
Vosram <63881577+vosram@users.noreply.github.com>
2026-01-19 10:20:26 -0600

    Add temp and speed converters

---
+++ lib/converters.py
@@ -0,0 +1,18 @@
+def kelvin_to_f(k_temp: float) -> str:
+    result = (k_temp * 1.8) - 459.67
+    return "{:.2f} F".format(result)
+
+
+def kelvin_to_c(k_temp: float) -> str:
+    result = k_temp - 273.15
+    return "{:.2f} C".format(result)
+
+
+def wind_speed_to_kmph(speed: float) -> str:
+    result = speed * 3.6
+    return "{:.2f} km/h".format(result)
+
+
+def wind_speed_to_mph(speed: float) -> str:
+    result = speed * 2.2369362921
+    return "{:.2f} mi/h".format(result)
