e87343de6065a51eafe53a13f5c2f06d69b533bd
Vosram <63881577+vosram@users.noreply.github.com>
2026-01-19 16:12:43 -0600

    Add 12Hours command

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from lib.formatters import (
     format_weather_desc,
     format_percipitation,
 )
+from lib.weatherrecord import WeatherRecord
 
 load_dotenv()
 OW_API_KEY = os.environ.get("OPEN_WEATHER_MAP_API_KEY", "")
@@ -182,6 +183,144 @@ def current(
         table.add_row("Rain", final_data["rain"])
     if final_data["snow"] is not None:
         table.add_row("Snow", final_data["snow"])
+    console.print(table)
+
+
+@app.command("12hours")
+def _12hours(
+    coords: Annotated[
+        str,
+        typer.Argument(help="<lat>,<lon>"),
+    ],
+    t_metric: Annotated[
+        str,
+        typer.Option(
+            "--t-metric",
+            "-t",
+            help="'c' for Celcius 'f' for Fahrenheit.",
+            show_default="Fahrenheit",
+        ),
+    ] = "f",
+    w_metric: Annotated[
+        str,
+        typer.Option(
+            "--w-metric",
+            "-w",
+            help="'m' for mph. 'k' for kmph.",
+            show_default="mph",
+        ),
+    ] = "m",
+):
+    # Interpret CLI options for query params
+    if t_metric != "c" and t_metric != "f":
+        print(
+            "[bold red]Error:[/bold red] --t-metric should be either 'f' or 'c'. Exiting..."
+        )
+        raise typer.Exit(1)
+
+    if w_metric != "m" and w_metric != "k":
+        print(
+            "[bold red]Error:[/bold red] --w-metric should be either 'm' or 'k'. Exiting..."
+        )
+        raise typer.Exit(1)
+
+    parsed_coords = coords.split(",")
+    weather_call_params = {
+        "appid": OW_API_KEY,
+        "exclude": "current,minutely,daily,alerts",
+        "lat": parsed_coords[0],
+        "lon": parsed_coords[1],
+    }
+    geo_call_params = {
+        "appid": OW_API_KEY,
+        "limit": 3,
+        "lat": parsed_coords[0],
+        "lon": parsed_coords[1],
+    }
+    full_weather_url = f"{OW_API_URL}/data/3.0/onecall"
+    full_geoloc_url = f"{OW_API_URL}/geo/1.0/reverse"
+
+    # Call weather API for current weather & Geolocation API
+    weather_res = requests.get(full_weather_url, params=weather_call_params)
+    if weather_res.status_code != 200:
+        print(
+            f"[bold red]Error:[/bold red] [red]request errored with code {res.status_code}[/red]"
+        )
+        raise typer.Exit(1)
+
+    geoloc_res = requests.get(full_geoloc_url, params=geo_call_params)
+    if geoloc_res.status_code != 200:
+        print(
+            f"[bold red]Error:[/bold red] [red]request errored with code {geloc_res.status_code}[/red]"
+        )
+        raise typer.Exit(1)
+
+    # Prepare Data for Console output
+    weather_data = weather_res.json()
+    geoloc_data = geoloc_res.json()
+
+    hourly_records = []
+
+    for i in range(12):
+        data = weather_data["hourly"][i]
+        wind_gust = None
+        rain = None
+        snow = None
+        pop = None
+        if "wind_gust" in data:
+            wind_gust = data["wind_gust"]
+        if "rain" in data:
+            rain = data["rain"]["1h"]
+        if "snow" in data:
+            snow = data["snow"]["1h"]
+        if "pop" in data:
+            pop = data["pop"]
+        record = WeatherRecord(
+            data["dt"],
+            data["temp"],
+            data["feels_like"],
+            data["humidity"],
+            data["wind_speed"],
+            data["weather"][0],
+            wind_gust=wind_gust,
+            rain=rain,
+            snow=snow,
+            pop=pop,
+            temp_units=t_metric,
+            wind_units=w_metric,
+        )
+        hourly_records.append(record)
+
+    print(
+        f"12 hour weather for [green]{geoloc_data[0]["name"]}, {geoloc_data[0]["state"]}, {geoloc_data[0]["country"]}[/green]"
+    )
+
+    table = Table(
+        "Datetime",
+        "Temp",
+        "Feels Like",
+        "Humidity",
+        "Wind Speed",
+        "Wind Gust",
+        "Condition",
+        "rain",
+        "snow",
+        "pop",
+    )
+    for i in range(12):
+        record = hourly_records[i]
+        table.add_row(
+            record.get_datetime("hourly"),
+            record.get_temp(),
+            record.get_feels_like(),
+            record.get_humidity(),
+            record.get_wind_speed(),
+            record.get_wind_gust(),
+            record.get_weather_condition(),
+            record.get_rain(),
+            record.get_snow(),
+            record.get_pop(),
+        )
     console.print(table)
 
 
