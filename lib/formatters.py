a9aaf65c573f9df87879ce4ed3f302bab5cf41cb
Vosram <63881577+vosram@users.noreply.github.com>
2026-01-19 10:19:37 -0600

    Add temp, speed, humidity, and time formatters

---
+++ lib/formatters.py
@@ -0,0 +1,29 @@
+from lib.converters import (
+    kelvin_to_c,
+    kelvin_to_f,
+    wind_speed_to_kmph,
+    wind_speed_to_mph,
+)
+from datetime import datetime
+
+
+def kelvin_to_formatted(k_temp: float, unit: "f" | "c") -> str:
+    if unit == "f":
+        return kelvin_to_f(k_temp)
+    else:
+        return kelvin_to_c(k_temp)
+
+
+def format_wind_speed(speed: float, unit: "m" | "k") -> str:
+    if unit == "k":
+        return wind_speed_to_kmph(speed)
+    else:
+        return wind_speed_to_mph(speed)
+
+
+def timestamp_to_fmt_text(timestamp: int) -> str:
+    return datetime.fromtimestamp(timestamp).strftime("%a %b %d, %Y %I:%M %p")
+
+
+def format_humidity(val: int) -> str:
+    return f"{val}%"
